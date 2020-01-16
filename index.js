1d5c7b07fa3f8a26e88d721033bf4304adc87552
Guy Thompson <guy.thompson@emishealth.com>
2020-01-16 14:36:41 +0000

    Added auth functionality to google
    Added auth functionality to google

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStratergy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send ({ hi: 'there' });
-});
+passport.use(
+    new GoogleStratergy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID || keys.googleClientID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
 
-app.get('/bye', (req, res) => {
-    res.send ({ bye: 'scrub' });
-});
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
