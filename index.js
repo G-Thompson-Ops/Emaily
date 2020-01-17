6c21331b78e06553c147581b71b19f1e1a6ad108
Guy Thompson <guy.thompson@emishealth.com>
2020-01-17 11:25:54 +0000

    Refactor
    Major refactor - splitting up code out of index

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStratergy = require('passport-google-oauth20').Strategy;
-//const keys = require('./config/keys'); No longer required as we are using env-vars
+require('./services/passport');
 
 const app = express();
 
-passport.use(
-    new GoogleStratergy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID, //can also use keys.googleClientID here is using keys.js method
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/auth/google/callback',
-        },
-        (accessToken, refrestToken, profile, done) => {
-            console.log('Access Token: ', accessToken);
-            console.log('Refresh Token: ', refrestToken);
-            console.log('Profile: ', profile);
-        }
-    )
-);
-
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get(
-    '/auth/google/callback',
-    passport.authenticate('google')
-);
+require('./routes/authroutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-app.enable("trust proxy");
+app.enable("trust proxy"); //Heroku SSL workaround
